d209581b98cb55df632ec4fb7a7a4295fa17f936
Abraham Hosch <hosch250@live.com>
2024-11-19 18:04:56 -0600

    Clean some stuff up

---
+++ DiscriminatedUnion.Generator.Examples/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DiscriminatedUnion.Generator.Examples;
+
+class Program
+{
+    public static void Main() { }
+}
+
+[DiscriminatedUnion]
+public abstract partial record Shape
+{
+    public partial record Circle(float Radius);
+    public partial record EquilateralTriangle(double SideLength);
+    public partial record Square(double SideLength);
+    public partial record Rectangle(double Height, double Width);
+
+    internal static double Area(Shape shape) =>
+        shape switch
+        {
+            // alt syntax: Circle { Radius: var radius } => Math.PI * radius * radius,
+
+            Circle(var radius) => Math.PI * radius * radius,
+            EquilateralTriangle(var side) => Math.Sqrt(3.0) / 4.0 * side * side,
+            Square(var side) => side * side,
+            Rectangle(var height, var width) => height * width,
+            _ => throw new NotImplementedException(),
+        };
+}
+
+[DiscriminatedUnion]
+abstract partial record Result
+{
+    internal partial record OK<T>(T Value);
+    internal partial record Error(string Message);
+}
