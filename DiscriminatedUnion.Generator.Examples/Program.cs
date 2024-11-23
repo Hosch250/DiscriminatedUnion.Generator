dd1cbf999539dc651d9ee69e5ab4eec31d2565b9
Abraham Hosch <hosch250@live.com>
2024-11-23 00:44:28 -0600

    Clean up code

--- DiscriminatedUnion.Generator.Examples/Program.cs
+++ DiscriminatedUnion.Generator.Examples/Program.cs
@@ -39,7 +39,6 @@ class Program
 }
 
 [DiscriminatedUnion(Serializable = true)]
-//[System.Text.Json.Serialization.JsonConverter(typeof(ShapeConverter1))]
 public abstract partial record Shape
 {
     public partial record Circle(float Radius);
@@ -64,57 +63,10 @@ public abstract partial record Shape
 }
 
 [DiscriminatedUnion(Serializable = true)]
-//[System.Text.Json.Serialization.JsonConverter(typeof(TreeConverter1))]
 public abstract partial record Tree
 {
     public partial record Node(Tree? Right = null, Tree? Left = null);
     public partial record Leaf(string Name);
-}
-
-
-internal sealed class TreeConverter1 : System.Text.Json.Serialization.JsonConverter<Tree>
-{
-    public override Tree? Read(ref System.Text.Json.Utf8JsonReader reader, System.Type typeToConvert, System.Text.Json.JsonSerializerOptions options)
-    {
-        var node = System.Text.Json.Nodes.JsonNode.Parse(ref reader);
-        foreach (var prop in typeof(Tree).GetProperties())
-        {
-            var value = node?[prop.Name];
-            if (value?.GetValueKind() == System.Text.Json.JsonValueKind.True)
-            {
-                var type = typeof(Tree).GetNestedType(prop.Name[2..]);
-                if (type is not null)
-                {
-                    return System.Text.Json.JsonSerializer.Deserialize(node, type) as Tree;
-                }
-            }
-        }
-
-        return null;
-    }
-
-    public override void Write(System.Text.Json.Utf8JsonWriter writer, Tree value, System.Text.Json.JsonSerializerOptions options)
-    {
-        writer.WriteStartObject();
-
-        foreach (var prop in value.GetType().GetProperties())
-        {
-            string propertyName = prop.Name;
-
-            var val = value.GetType().GetProperty(propertyName)?.GetValue(value);
-            if (val is not null)
-            {
-                writer.WritePropertyName(options.PropertyNamingPolicy?.ConvertName(propertyName) ?? propertyName);
-                System.Text.Json.JsonSerializer.Serialize(writer, val, options);
-            }
-            else
-            {
-                writer.WriteNull(propertyName);
-            }
-        }
-
-        writer.WriteEndObject();
-    }
 }
 
 [DiscriminatedUnion]
